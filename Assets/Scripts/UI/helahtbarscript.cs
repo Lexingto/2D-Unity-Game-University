a4a0463c91f1f6d1d23d769114e6f891f71f5dde
Lexingto <74386589+Lexingto@users.noreply.github.com>
2021-05-13 01:16:40 +0100

    .

---
+++ Assets/Scripts/UI/helahtbarscript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpBarSetUp : MonoBehaviour
+{
+    Image JumpBar;
+    float maxCool = 100f; //the max amount of health
+    public static float CoolDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JumpBar = GetComponent<Image>(); //displays the healthbar image
+    CoolDown = maxCool; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpBar.fillAmount = CoolDown / maxCool; //health divided by the max amount you can have
+    }
+}
